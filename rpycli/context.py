bc8b7b286d387158106fa021d648639e84f8a901
Richard Cook <rcook@rcook.org>
2024-05-21 23:42:38 -0700

    Handle list arguments

--- rpycli/context.py
+++ rpycli/context.py
@@ -31,7 +31,10 @@ class ContextMeta(type):
 
         def log(self, log_level, *args, **kwargs):
             module = get_calling_module(inspect.stack())
-            logger = self._logger(module.__name__)
+            logger = self.__class__._get_logger(
+                context_name=self.name,
+                log_level=self.log_level,
+                name=module.__name__)
             method = getattr(logger, log_level)
             return method(*args, **kwargs)
 
@@ -49,6 +52,12 @@ class Context(metaclass=ContextMeta):
 
     @classmethod
     def from_args(cls, args, name=None):
+        def encode_arg_value(obj):
+            match obj:
+                case list() as items:
+                    return f"[{', '.join(encode_arg_value(item) for item in items)}]"
+                case _: return str(obj)
+
         d = args.__dict__.copy()
         log_level = logging.getLevelNamesMapping()[d.pop("log_level").upper()]
         for k in SKIP_ARGS:
@@ -62,7 +71,8 @@ class Context(metaclass=ContextMeta):
 
         ctx = ctx_cls(name=name, log_level=log_level, **d)
         for k in sorted(args.__dict__.keys() - SKIP_ARGS):
-            ctx.log_info(f"{k} = {args.__dict__[k]}")
+            s = encode_arg_value(args.__dict__[k])
+            ctx.log_info(f"{k} = {s}")
 
         return ctx
 
@@ -84,9 +94,10 @@ class Context(metaclass=ContextMeta):
             raise
 
     @cache
-    def _logger(self, name):
-        name = self.name \
-            if name == "__main__" and self.name is not None \
+    @staticmethod
+    def _get_logger(context_name, log_level, name):
+        name = context_name \
+            if name == "__main__" and context_name is not None \
             else name
         formatter = logging.Formatter(
             Fore.LIGHTMAGENTA_EX + "[%(asctime)s] " +
@@ -95,9 +106,9 @@ class Context(metaclass=ContextMeta):
             Fore.LIGHTGREEN_EX + "%(message)s" +
             Style.RESET_ALL)
         handler = logging.StreamHandler()
-        handler.setLevel(self.log_level)
+        handler.setLevel(log_level)
         handler.setFormatter(formatter)
         logger = logging.getLogger(name)
-        logger.setLevel(self.log_level)
+        logger.setLevel(log_level)
         logger.addHandler(handler)
         return logger
