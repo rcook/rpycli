6e4d136988fbc584d43a9b74302376f1f0bdded0
Richard Cook <rcook@rcook.org>
2024-05-26 15:22:42 -0700

    Annotate cprint module

--- rpycli/cprint.py
+++ rpycli/cprint.py
@@ -1,9 +1,10 @@
 from colorama import Style
 from io import StringIO
+from typing import Any
 import sys
 
 
-def cprint(fore, *args, **kwargs):
+def cprint(fore: str, *args: Any, **kwargs: Any) -> None:
     file = kwargs.pop("file", sys.stdout)
     with StringIO() as stream:
         print(*args, **kwargs, file=stream)
