b9d2960f46d1989f2f136e788cbbd03c8a84348c
Richard Cook <rcook@rcook.org>
2024-05-26 09:27:34 -0700

    Annotate fs module

--- rpycli/fs.py
+++ rpycli/fs.py
@@ -1,25 +1,32 @@
 from pathlib import Path
 from shutil import which
+from typing import Iterable
 import os
 import platform
 
 
-def find_program(name):
+def find_program(name: str) -> Path:
     result = which(name)
     if result is None:
         raise RuntimeError(f"Program \"{name}\" not found")
     return Path(result).resolve()
 
 
-def home_dir():
+def home_dir() -> Path:
+    def getenv_must_exist(key: str) -> str:
+        value = os.getenv(key)
+        if value is not None:
+            return value
+        raise RuntimeError(f"Environment variable {key} not defined")
+
     s = platform.system()
     match s.lower():
-        case "darwin": return Path(os.getenv("HOME")).resolve()
-        case "windows": return Path(os.getenv("USERPROFILE")).resolve()
+        case "darwin": return Path(getenv_must_exist("HOME")).resolve()
+        case "windows": return Path(getenv_must_exist("USERPROFILE")).resolve()
         case _: raise NotImplementedError(f"Unsupported platform \"{s}\"")
 
 
-def clean_dir(dir, fail_ok=False):
+def clean_dir(dir: Path, fail_ok: bool = False) -> None:
     for root, ds, _ in dir.walk(top_down=False):
         ds.sort()
         for d in ds:
@@ -34,7 +41,7 @@ def clean_dir(dir, fail_ok=False):
                     p.rmdir()
 
 
-def iter_files(start_dir, include_suffixes=None, ignore_dirs=None):
+def iter_files(start_dir: Path, include_suffixes: Iterable[str] | None = None, ignore_dirs: Iterable[str] | None = None) -> Iterable[Path]:
     include_suffixes = {x.lower() for x in include_suffixes} \
         if include_suffixes is not None \
         else None
