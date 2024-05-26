e26d6d6d7d031cf12b3611bf20f2f07350b9e769
Richard Cook <rcook@rcook.org>
2024-05-26 14:42:57 -0700

    Annotate invoke module

--- rpycli/invoke.py
+++ rpycli/invoke.py
@@ -1,7 +1,11 @@
 from inspect import getfullargspec
+from typing import Any, Callable, TypeVar
 
 
-def invoke_func(func, **kwargs):
+T = TypeVar("T")
+
+
+def invoke_func(func: Callable[..., T], **kwargs: Any) -> T:
     spec = getfullargspec(func)
     d = {name: kwargs.pop(name) for name in spec.args}
     kwargs = {} if spec.varkw is None else kwargs
