83cea683f05fabc97ca5bbd3bba9a51bb310470c
Richard Cook <rcook@rcook.org>
2024-05-26 15:23:37 -0700

    Annotate error module

--- rpycli/error.py
+++ rpycli/error.py
@@ -1,10 +1,13 @@
+from typing import Any
+
+
 class ReportableError(RuntimeError):
-    def __init__(self, *args, exit_code=None, **kwargs):
+    def __init__(self, *args: Any, exit_code: int | None = None, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self._exit_code = 1 if exit_code is None else exit_code
 
     @property
-    def exit_code(self): return self._exit_code
+    def exit_code(self) -> int: return self._exit_code
 
 
 class UserCancelledError(RuntimeError):
